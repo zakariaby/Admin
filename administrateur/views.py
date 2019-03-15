ab61e634bda6cadb95d6af972593772d58145d30
zakariaby <zakariaby24@gmail.com>
2019-03-15 17:38:46 +0000

    connexion online

---
+++ administrateur/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+def blank(request):
+	return render (request,'blank.html')
+
+def home(request):
+	return render (request,'home.html')
+
+# Create your views here.
+def chart(request):
+	return render (request,'chart.html')
+
+
+# Create your views here.
+def form(request):
+	return render (request,'form.html')
+
+
+# Create your views here.
+def index(request):
+	return render (request,'index.html')
+
+
+# Create your views here.
+def table(request):
+	return render (request,'table.html')
+
+
+# Create your views here.
+def ui(request):
+	return render (request,'ui.html')
+
+def tab_panel(request):
+	return render (request,'tab_panel.html')
+
+
+# Create your views here.
+
+
+
+# Create your views here.
